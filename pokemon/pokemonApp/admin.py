bbbfc43a3fb3056672f44ed0eebc92ba7bd97fe3
karlosA99 <karlos.bruno99@gmail.com>
2022-06-12 01:06:25 -0400

    admin registration

--- pokemon/pokemonApp/admin.py
+++ pokemon/pokemonApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from pokemonApp.models import CaughtPokemon, Citizen, City, Community, Duel, Element, Gym, Motion, Pokemon, Region, Species, Trainer, Village
 
 # Register your models here.
+
+admin.site.register(Region)
+admin.site.register(City)
+admin.site.register(Element)
+admin.site.register(Community)
+admin.site.register(Village)
+admin.site.register(Citizen)
+admin.site.register(Gym)
+admin.site.register(Motion)
+admin.site.register(Trainer)
+admin.site.register(Species)
+admin.site.register(Pokemon)
+admin.site.register(CaughtPokemon)
+admin.site.register(Duel)
