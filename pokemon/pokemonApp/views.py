30312e52c4139108c3c68e4ed47fb43d8bd89df3
Sheyla <sheylvsz01@gmail.com>
2022-06-18 15:58:12 -0400

    filters at citizens working except region

--- pokemon/pokemonApp/views.py
+++ pokemon/pokemonApp/views.py
@@ -1,12 +1,9 @@
-from unicodedata import name
-from cv2 import CirclesGridFinderParameters, RQDecomp3x3
 from django.db import reset_queries
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.views.generic import ListView
 from pokemonApp.models import *
-from django_filters import CharFilter
 
 
 def index(request):
@@ -25,10 +22,13 @@ class CitizenList(ListView):
             citizen = citizen.filter(age= int(request.GET["age"]))
         if "gender" in request.GET and request.GET["gender"] != '':
             citizen = citizen.filter(sex= request.GET["gender"])
+
+            
         if "region" in request.GET and request.GET['region'] !='':
-            citizen = citizen.filter(born_region=request.GET(['region']))
+            citizen = citizen.filter(born_region__name=request.GET(["region"]))
 
         return render(request, self.template_name, {'object_list': citizen})
+        
 class TrainerList(ListView):
     model = Trainer
     template_name = 'trainer/trainer.html'
